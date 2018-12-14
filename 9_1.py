c5089827e16931f1846e1da1123f352c197b5139
= <=>
2018-12-14 12:56:15 -0500

    finished day 9

--- 9_1.py
+++ 9_1.py
@@ -1,5 +1,5 @@
-players = 9
-highest_marble = 25
+players = 400
+highest_marble = 7186400
 scores = [0 for x in range(players+1)]
 
 circle = []
@@ -11,19 +11,22 @@ turn = 0
 def add_marble(circle, marble):
     global current_marble
     global turn
+    if turn % 100 == 0:
+        print(float(turn)/float(highest_marble))
     if marble % 23 == 0 and turn > 0:
         index = turn % players if turn % players else players
-        print("Player: "+str(index))
-        print("Marble: "+str(marble) + ' Index: '+str(circle.index(current_marble)))
+        # print("Player: "+str(index))
+        # print("Marble: "+str(marble) + ' Index: '+str(circle.index(current_marble)))
         scores[index] += marble
         to_pop = circle.index(current_marble) - 7
+        current_marble = circle[to_pop + 1]
         marble = circle.pop(to_pop)
-        print("Marble: "+str(marble) + ' Index: '+str(to_pop))
+        # print("Marble: "+str(marble) + ' Index: '+str(to_pop))
         scores[index] += marble
-        print("Score: "+str(scores[index]))
-        current_marble = circle[to_pop]
-        print("Current Marble: "+str(current_marble))
-        print("")
+        # print("Score: "+str(scores[index]))
+        # print("Current Marble: "+str(current_marble))
+        # print(circle)
+        # print("")
     else:
         if len(circle) < 2:
             index = 0
@@ -35,11 +38,12 @@ def add_marble(circle, marble):
             else:
                 circle.insert(index, marble)
         current_marble = marble
-# print(str(turn) + ' ' + ','.join(str(x) for x in circle) + ' -' + str(current_marble))
+    # print(str(turn) + ' ' + ','.join(str(x) for x in circle) + ' -' + str(current_marble))
     turn += 1
 
 
 while len(marbles):
     add_marble(circle, marbles.pop(0))
-
-print(scores)
+# print(max(circle))
+# print(circle)
+print(max(scores))
